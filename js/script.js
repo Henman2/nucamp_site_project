609015323e560e68dec2976c6eba05c691f94aaa
Henman2 <47754370+Henman2@users.noreply.github.com>
2023-11-04 17:33:39 +0000

    Add Weather Widget feature

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ console.log('javascript connected!');
 const carousel = new bootstrap.Carousel('#homeCarousel', {
     interval: 2000,
     pause: false
-})
+});
 
+//declare variables
 const carouselButton = document.getElementById('carouselButton');
 const faIcon = document.getElementById('faButton');
-
-
+const weatherInfo = document.getElementById('weather');
 // when the pause button is clicked, pause the carousel
 /*
 const carouselPause = document.getElementById('carouselPause');
@@ -17,7 +17,6 @@ carouselPause.addEventListener('click', function() {
     carousel.pause();
 })
 */
-
 // when the play button is clicked, begin cycling through the images
 /*
 const carouselPlay = document.getElementById('carouselPlay');
@@ -37,3 +36,37 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 })
+
+// weather component
+// weatherInfo.addEventListener('load', fetchWeather);
+
+async function fetchWeather(){
+    const apiKey = process.env.Weather_API_Key;
+    const city = "calgary";
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try{
+        const response = await fetch(apiURL);
+        const weatherData = await response.json();
+        console.log(weatherData);
+        displayWeather(weatherData);
+
+    }
+    catch(err){
+        console.error(err);
+    }
+}
+function displayWeather(weatherdata){
+    const temp = document.getElementById('weather-temp');
+    const weatherDescription = document.getElementById('weather-description');
+    const icons = document.getElementById('weather-icon');
+    temp.textContent = `${weatherdata.main.temp}°C`;
+    weatherDescription.textContent = weatherdata.weather[0].description;
+    let iconURL = `https://openweathermap.org/img/wn/${weatherdata.weather[0].icon}.png`;
+    ;
+
+   icons.innerHTML  = `<img src="${iconURL}" alt="icons">`;
+    console.log(weatherDescription.textContent);
+    console.log(temp.textContent);
+}
+fetchWeather();
